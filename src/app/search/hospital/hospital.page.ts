6dd68f3ba0a7b6330fee85223f7a802059d80d32
Ramyarnayak <ramya.nayak@ymedialabs.com>
2021-03-30 16:01:13 +0530

    second commit

--- src/app/search/hospital/hospital.page.ts
+++ src/app/search/hospital/hospital.page.ts
@@ -9,6 +9,7 @@ import { PlacesService } from 'src/app/places/places.service';
 import { Place, searchPlace } from 'src/app/places/place.model';
 import { CreateBookingComponent } from 'src/app/bookings/create-booking/create-booking.component';
 import { BookingService } from 'src/app/bookings/booking.service';
+import { HintModalPage } from '../hint-modal/hint-modal.page';
 
 
 declare var google: any;
@@ -26,28 +27,25 @@ export class HospitalPage implements OnInit, OnDestroy {
   isLoading = false;
   private placesSub: Subscription;
 
-  constructor(private placesService: PlacesService, private router: Router, private loadingCtrl: LoadingController) {}
+  constructor(private placesService: PlacesService,
+    public modalCtrl: ModalController, private router: Router, private loadingCtrl: LoadingController) {}
 
   ngOnInit() {
     this.placesSub = this.placesService.searchplaces.subscribe(places => {
       this.results = places;
-      
       console.log("place===" + JSON.stringify(places))
-    });
-    
+    });   
   }
 
   ionViewWillEnter() {
     this.isLoading = true;
     this.placesService.fetchSearchPlaces().subscribe(() => {
       this.isLoading = false;
-      console.log(this.placesService.fetchSearchPlaces())
-      
+      console.log(this.placesService.fetchSearchPlaces())     
     });
   }
 
   onCancel(placeId: string) {
-
     console.log("yws")
     this.loadingCtrl.create({ message: 'Cancelling...' }).then(loadingEl => {
       loadingEl.present();
@@ -57,8 +55,19 @@ export class HospitalPage implements OnInit, OnDestroy {
     });
   }
 
+  openHintModal() {
+    console.log("start")
+    this.openModal(HintModalPage, ['inset-modal']);
+    console.log("done")
+  }
  
-
+  async openModal(pageName, css: string[]) {
+    const modal = await this.modalCtrl.create({
+      component: pageName,
+      cssClass: css // Global.scss
+    });
+    await modal.present();
+  }
   ngOnDestroy() {
     if (this.placesSub) {
       this.placesSub.unsubscribe();
